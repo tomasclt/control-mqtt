a810d8fcb086538f2a0478949809af77fc0751ea
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-09-27 16:00:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 import streamlit as st
 import json
 values = 0.0
+act1=""
 
 def on_publish(client,userdata,result):             #create function for callback
     print("el dato ha sido publicado \n")
