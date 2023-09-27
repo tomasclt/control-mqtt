c7c6d23817c184e692748d8af7fb3fcdae2af99f
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-09-27 14:33:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as paho
 import time
 import streamlit as st
+import json
 
 def on_publish(client,userdata,result):             #create function for callback
     print("el dato ha sido publicado \n")
